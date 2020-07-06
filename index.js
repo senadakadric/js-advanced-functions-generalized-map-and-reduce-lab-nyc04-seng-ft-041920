d6a163ef81ddc798cd2669425fccf2eda8c27a5b
senadakadric <skadric@fordham.edu>
2020-07-05 21:32:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+
+function map(array, fN) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(fN(array[i]));
+  }
+  return newArray;
+}
+
+function reduce(array, fN, startingValue) {
+  let value = null;
+  if (startingValue === undefined) {
+    value = array[0];
+  } else {
+    value = startingValue;
+  }
+  //   for (let i = 0; i < array.length; i++) {
+  //     // value += array[i];
+  //     value = fN(array[i], value);
+  //   }
+  let i = startingValue ? 0 : 1;
+  while (i < array.length) {
+    value = fN(array[i], value);
+    i++;
+  }
+  return value;
+}
